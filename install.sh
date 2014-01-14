9dbd72c1038aa0641fd4fb2e0c60003aa859fb76
josh <josh@josh.(none)>
2014-01-14 08:09:59 -0800

    adding extras and git

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-sudo apt-get install -y git gitk kdiff3 pkg-config build-essential autogen autoconf pkg-config
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git
+
+sudo apt-get install -y git gitk kdiff3 pkg-config build-essential autogen autoconf pkg-config vlc
+
+sudo apt-get install ubuntu-restricted-extras
